d646926ef0cc37c9e30e25421c35c45b9acf911e
syrup3663ve <72807083+syrup3663ve@users.noreply.github.com>
2023-05-27 20:37:15 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Top from "./Top";
+
+function App() {
+    return (
+        <div className="App">
+            Hello React!
+            <Top/>
+        </div>
+    );
+}
+
+export default App;
